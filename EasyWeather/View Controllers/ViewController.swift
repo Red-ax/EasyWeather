948756ee63659456f232145ef5e3adfff647caa6
Redax <deev.go@gmail.com>
2021-05-09 06:27:19 +0700

    add popup view

--- EasyWeather/View Controllers/ViewController.swift
+++ EasyWeather/View Controllers/ViewController.swift
@@ -12,11 +12,19 @@ class ViewController: UIViewController {
     let networkManager = NetworkManager()
     
     var alertButton = UIButton()
+    var popupButton = UIButton()
+    
+    let popupView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(white: 0.9, alpha: 0.9)
+        return view
+    }()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         networkManager.fetchData(city: "Moscow")
         
         customElevents()
@@ -25,15 +33,32 @@ class ViewController: UIViewController {
         
     }
     
+    //MARK: - Actions
     @objc func serchPressed(_ sender: UIAction) {
         
         presentSerchAlertController(title: "Enter city name", message: nil, style: .alert) { city in
             self.networkManager.fetchData(city: city)
         }
-        
     }
-
     
+    @objc func serchPressedPopupView(_ sender: UIAction) {
+        
+        UIView.animate(withDuration: 1, delay: 0.3, options: .allowAnimatedContent) {
+            self.popupView.transform = CGAffineTransform(translationX: 0, y: -self.popupView.frame.size.height)
+        }
+    }
+    
+    
+    // MARK: - touchesBegan
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        UIView.animate(withDuration: 1, delay: 0.3, options: .curveEaseInOut) {
+            self.popupView.transform = CGAffineTransform(translationX: 0, y: self.popupView.frame.size.height)
+        }
+    }
+    
+
+    // MARK: - Methods
     func customElevents() {
 
         // alertButton
@@ -43,10 +68,26 @@ class ViewController: UIViewController {
         alertButton.setTitleColor(.white, for: .normal)
         alertButton.setTitleColor(.systemGray4, for: .highlighted)
         alertButton.titleLabel?.font = UIFont.systemFont(ofSize: 24)
-        
         alertButton.addTarget(self, action: #selector(serchPressed), for: .touchUpInside)
         
         view.addSubview(alertButton)
+        
+        // alertButton
+        popupButton.layer.cornerRadius = 8
+        popupButton.backgroundColor = .systemRed
+        popupButton.setTitle("Hello View", for: .normal)
+        popupButton.setTitleColor(.white, for: .normal)
+        popupButton.setTitleColor(.systemGray4, for: .highlighted)
+        popupButton.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+        
+        popupButton.addTarget(self, action: #selector(serchPressedPopupView), for: .touchUpInside)
+        
+        view.addSubview(popupButton)
+        
+        // actionView
+        popupView.layer.cornerRadius = 12
+        view.addSubview(popupView)
+        
 
         
     }
@@ -60,6 +101,18 @@ class ViewController: UIViewController {
         alertButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
         alertButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
+        // viewButton
+        popupButton.translatesAutoresizingMaskIntoConstraints = false
+        popupButton.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        popupButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        popupButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        popupButton.topAnchor.constraint(equalTo: alertButton.bottomAnchor, constant: 60).isActive = true
+        
+        // actionView
+        popupView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        popupView.heightAnchor.constraint(equalToConstant: view.frame.height / 2).isActive = true
+        popupView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: view.frame.size.height / 2).isActive = true
+
     }
 
 
